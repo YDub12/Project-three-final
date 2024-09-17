f211f240be2dd11940ca5e168853106b15db97da
YDub12 <ydubina@googlemail.com>
2024-09-17 13:28:03 +0000

    add a welcome message and username field

--- run.py
+++ run.py
@@ -1,11 +1,26 @@
 import random 
 
 # Constants
+BOARD_SIZE = 5
+SHIP_SIZES = [3, 2, 1, 1]
 
 # Symbols for the board
 
 # Create an empty board
 
 # Welcome message 
+def welcome_message(username):
+    print(f"Welcome Admiral {username}, to Battleships!\n")
+    print(f"You will be playing on a {BOARD_SIZE}X{BOARD_SIZE} grid.\n")
+    print("Your goal is to sink all of your opponents ships. \n")
+    print("You will do this by making guesses below.")
 
 # Main function
+def main():
+    # Get username 
+    username = input("Enter your username here: ")
+    welcome_message(username)
+
+
+if __name__ == "__main__":
+    main()

ca4701257d40d7714c93f4f655af7a21c8003bf2
YDub12 <ydubina@googlemail.com>
2024-09-17 13:22:08 +0000

    Initial commit with bare bones structure

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+# Constants
+
+# Symbols for the board
+
+# Create an empty board
+
+# Welcome message 
+
+# Main function
